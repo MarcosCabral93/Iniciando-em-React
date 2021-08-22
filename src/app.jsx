c89200e58b344a7bd5cb71dfa50295b9bc984c0c
Marcos Paulo M Cabral <69735297+MarcosCabral93@users.noreply.github.com>
2021-08-22 15:21:45 -0300

    iniciando o estudo dos Forms em React

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,40 @@
 import React, { Component } from 'react'
 
-import Main from './componentes/Main'
+
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      form:{
+      email: "",
+      senha: "",
+      sexo: ""
+    }}
+    this.trocaForm=this.trocaForm.bind(this)
     
+  }
+    //o name dos inputs tem que ser o mesmo do state
+    trocaForm(e){
+        let form=this.state.form;
+        form[e.target.name]= e.target.value
+        this.setState({form:form})
+          }
     render() { 
         return (
             
           <div>
-           
-              <Main />
-            
+           <input type="email" name="email" value={this.state.form.email}
+            onChange={this.trocaForm}/>
+           <input type="password" name="senha" value={this.state.form.senha}
+            onChange={this.trocaForm}/>
+            <select  name="sexo" value={this.state.form.sexo} onChange={this.trocaForm} >
+              <option value="Masculino">Masculino</option>
+              <option value="Feminino">Feminino</option>
+            </select>
+            <h3>{this.state.form.email}</h3>
+            <h3>{this.state.form.senha}</h3>
+            <h3>{this.state.form.sexo}</h3>
           </div>
         )}
 }

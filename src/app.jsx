e7c303528c472d7407ce6b3df59d461a3b67df61
Marcos Paulo M Cabral <69735297+MarcosCabral93@users.noreply.github.com>
2021-08-24 20:48:31 -0300

    Criando um TodoApp

--- src/app.jsx
+++ src/app.jsx
@@ -1,52 +1,16 @@
-import React, { Component } from 'react'
-import "./style.css"
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-       lista:[] 
-       }
-  }
-componentDidMount(){
-  let url='https://sujeitoprogramador.com/rn-api/?api=posts'
-  fetch(url)
-  .then((resposta)=>resposta.json())
-  .then((json)=>{
-    let state=this.state
-    state.lista=json;
-    this.setState(state)
-    console.log(json)
-  })
-  .catch((e)=>console.log(e))
-  
+import React, { Component } from 'react';
+import TodoList from './components/TodoList';
 
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Lista de Tarefas</h1>
+                <TodoList/>
+            </div>
+        )
+    }
 }
- 
-  render() { 
-    return ( 
-    
-    
-    <div>
-      <header>
-          React Dicas
-      </header>
 
-
-      {this.state.lista.map((item)=>{
-        return (
-
-        <div className="container">
-
-  
-        <article className="artigo">
-        <h2 className="titulo">{item.titulo}</h2>
-         <img className="img" src={item.capa} alt="Capa"/>
-          <p className="subtitulo">{item.subtitulo}</p>
-        </article>
-        </div>)
-      })}
-    </div> );
-  }
-}
- 
 export default App;

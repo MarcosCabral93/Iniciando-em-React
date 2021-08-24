12e778b4367c356179730c7c593440f0c689fb40
Marcos Paulo M Cabral <69735297+MarcosCabral93@users.noreply.github.com>
2021-08-24 17:18:11 -0300

    refactor criando a aplicação com o Fetch

--- src/app.jsx
+++ src/app.jsx
@@ -1,61 +1,52 @@
-import React, { Component } from 'react';
-import imgd from './assets/images/cronometro.png'
-import './style.css'
-
+import React, { Component } from 'react'
+import "./style.css"
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
+  constructor(props){
+    super(props)
+    this.state = {
+       lista:[] 
+       }
+  }
+componentDidMount(){
+  let url='https://sujeitoprogramador.com/rn-api/?api=posts'
+  fetch(url)
+  .then((resposta)=>resposta.json())
+  .then((json)=>{
+    let state=this.state
+    state.lista=json;
+    this.setState(state)
+    console.log(json)
+  })
+  .catch((e)=>console.log(e))
+  
 
-            cronometro: 0,
-            botao:"start"
-        };
-        this.temporizador=null
-        this.iniciar=this.iniciar.bind(this)
-        this.finalizar=this.finalizar.bind(this)
-    } 
-    iniciar(){
-        let state=this.state
-        if(this.temporizador!=null){
-            clearInterval(this.temporizador)
-            this.temporizador=null
-           state.botao="start"
-         }else{
-            //renderiza a cada 1 milisguno
-            this.temporizador=setInterval(()=>{
-                let state=this.state
-                state.cronometro+=0.1
-                this.setState(state)
-            },100)
-            state.botao="pausar"
-        }
-        
-    }
-
-    finalizar(){
-        if(this.temporizador!=null){
-            clearInterval(this.temporizador)
-            this.temporizador=null}
-        let state=this.state
-        state.botao="start"
-        state.cronometro=0
-        this.setState(state)
-    }
-    render() { 
-        return (
-            <div className="container">
-               <div>
-                   <h1 className="titulo">Cronometrando com React</h1>
-                   <img src={imgd} alt="cronometro" className="imagem" />
-                   <h2 className="texto">{this.state.cronometro.toFixed(1)}</h2>
-               </div>
-                <div> 
-                    <button onClick={this.iniciar}>{this.state.botao}</button>
-                    <button onClick={this.finalizar}>Zerar</button>
-                </div>
-            </div>   
-        );
-    }
 }
  
-export default App ;
+  render() { 
+    return ( 
+    
+    
+    <div>
+      <header>
+          React Dicas
+      </header>
+
+
+      {this.state.lista.map((item)=>{
+        return (
+
+        <div className="container">
+
+  
+        <article className="artigo">
+        <h2 className="titulo">{item.titulo}</h2>
+         <img className="img" src={item.capa} alt="Capa"/>
+          <p className="subtitulo">{item.subtitulo}</p>
+        </article>
+        </div>)
+      })}
+    </div> );
+  }
+}
+ 
+export default App;
